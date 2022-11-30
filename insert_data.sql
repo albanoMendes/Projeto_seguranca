8ebd76452ca4d8bf0d270f56e8b1eb9f204be9e9
vdella <vitor.origamer@gmail.com>
2022-11-29 23:28:02 -0300

    script for inserting data

---
+++ insert_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Candidate (candidateID, name, party)
+    VALUES (0, 'Zé da Padaria', 'Padaria'),
+           (1, 'Maria Ana', 'Partido Importante'),
+           (2, 'Peter Pan', 'Partido do Nunca'),
+           (3, 'Capitão Gancho', 'Piratas');
