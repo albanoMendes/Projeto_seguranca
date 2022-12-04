2b986f3eb3048684431ec1c9ae4052b26210ba08
vdella <vitor.origamer@gmail.com>
2022-12-04 00:01:30 -0300

    added module to handle vote enc/decoding

--- src/crypto.py
+++ src/crypto.py
@@ -1,27 +1,16 @@
-from phe import paillier, EncodedNumber
+from phe import paillier, PaillierPublicKey, PaillierPrivateKey
+from pickle import dumps, loads
+from base64 import b64encode, b64decode
 
 
-num1 = 10
-num2 = 20
+def encrypt(vote: int, public_key: PaillierPublicKey) -> bytes:
+    vote = public_key.encrypt(vote)
+    vote = dumps(vote)
+    return b64encode(vote)
 
-pub_key, priv_key = paillier.generate_paillier_keypair()
-p1, p2 = paillier.generate_paillier_keypair()
-print(p1 == pub_key)
 
-cipher_num1, cipher_num2 = pub_key.encrypt(num1), pub_key.encrypt(num2)
-print(cipher_num1)
+def decrypt(vote: bytes, private_key: PaillierPrivateKey) -> int:
+    vote = b64decode(vote)
+    vote = loads(vote)
+    return private_key.decrypt(vote)
 
-# add two encrypted numbers together
-result = cipher_num1 + cipher_num2
-result = priv_key.decrypt(result)
-print("add two encrypted numbers together:", result)
-
-# add an encrypted number to a plaintext number
-result = cipher_num1 + 5
-result = priv_key.decrypt(result)
-print("add an encrypted number to a number:", result)
-
-# multiply an encrypted number by a plaintext number
-result = cipher_num1 * 10
-result = priv_key.decrypt(result)
-print("multiply an encrypted number to a number:", result)
