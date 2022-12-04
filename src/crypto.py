2a8d1e4f0c8c518b6304078111f587f12ed15863
vdella <vitor.origamer@gmail.com>
2022-12-04 20:34:30 -0300

    removed b64 enc/decoding

--- src/crypto.py
+++ src/crypto.py
@@ -1,16 +1,12 @@
-from phe import paillier, PaillierPublicKey, PaillierPrivateKey
+from phe import PaillierPublicKey, PaillierPrivateKey
 from pickle import dumps, loads
-from base64 import b64encode, b64decode
 
 
 def encrypt(vote: int, public_key: PaillierPublicKey) -> bytes:
     vote = public_key.encrypt(vote)
-    vote = dumps(vote)
-    return b64encode(vote)
+    return dumps(vote)
 
 
 def decrypt(vote: bytes, private_key: PaillierPrivateKey) -> int:
-    vote = b64decode(vote)
     vote = loads(vote)
     return private_key.decrypt(vote)
-
